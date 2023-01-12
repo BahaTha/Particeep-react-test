29126f58db8c85ea466ced0a71c2e08554a83c42
BahaTha <baha.thabet@live.ca>
2023-01-12 15:37:31 +0100

    Final version

---
+++ test_react/src/components/index.js
@@ -0,0 +1,6 @@
+import { default as MoviesList } from "./movies-list/movies-list"
+import { default as Card } from "./card/card"
+import { default as ImageWithLoading } from "./img-with-loading/img-with-loading"
+import { default as LikeDislikeButton } from "./like-dislike-button/like-dislike-button"
+import { default as DefaultButton } from "./default-button/default-button"
+export { MoviesList, Card, ImageWithLoading, LikeDislikeButton, DefaultButton }
